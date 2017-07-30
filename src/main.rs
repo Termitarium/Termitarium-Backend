2fadfd4e6f06b463baf05407eb392c480bd5e434
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2017-07-30 16:44:04 +0200

    Return a Hello world JSON

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
+// main.rs
 extern crate iron;
 
 use iron::prelude::*;
 use iron::status;
+use iron::mime::Mime;
 
 fn main() {
-    Iron::new(|_: &mut Request| {
-        Ok(Response::with((status::Ok, "Hello World!")))
-    }).http("localhost:3000").unwrap();
+  Iron::new(|_: &mut Request| {
+    let content_type = "application/json".parse::<Mime>().unwrap();
+
+    Ok(Response::with((content_type, status::Ok, "{ \"response\": \"So Cool!\" }")))
+  }).http("localhost:3002").unwrap();
 }
