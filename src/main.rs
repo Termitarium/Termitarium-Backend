258d164fccacdf6e1a698e3565e93ee8f9dbe339
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2017-11-11 19:23:19 +0100

    A new era beggins

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,3 @@
-// main.rs
-extern crate iron;
-
-use iron::prelude::*;
-use iron::status;
-use iron::mime::Mime;
-
 fn main() {
-  Iron::new(|_: &mut Request| {
-    let content_type = "application/json".parse::<Mime>().unwrap();
 
-    Ok(Response::with((content_type, status::Ok, "{ \"response\": \"So Cool!\" }")))
-  }).http("localhost:3002").unwrap();
 }
