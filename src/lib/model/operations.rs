97cd95af11ed0ea6f46d83b388b566afd01920de
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2017-11-12 18:49:14 +0100

    Derive serde traits on Operations

--- src/lib/model/operations.rs
+++ src/lib/model/operations.rs
@@ -1,9 +1,11 @@
 use super::{EntityId, EntityName, EntityType};
 
+#[derive(Serialize, Deserialize, Debug)]
 pub enum Operation {
     EntityOperation(EntityOperation)
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 pub enum EntityOperation {
     NewEntity{id: EntityId, name: EntityName, entity_type: EntityType}
 }
