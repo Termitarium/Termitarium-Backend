5680ad1552b3b137ee22eaa4ceae5e37bdaecdc2
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2017-11-12 15:07:03 +0100

    Add EntityType to model

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,21 +1,33 @@
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Model {
     // TODO: make fields private?
-    pub entities: Vec<Entity>
+    pub entities: Vec<Entity>,
+    pub entity_types: Vec<EntityType>
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Entity {
     // TODO: make fields private?
     pub id: EntityId,
+    pub entity_type: EntityType,
     pub name: EntityName
 }
 
 impl Entity {
-    fn new(id: EntityId, name: EntityName) -> Self {
-        Entity {id, name}
+    // TODO: builder pattern here
+    fn new(id: EntityId, entity_type: EntityType, name: EntityName) -> Self {
+        Entity {id, entity_type, name}
     }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct EntityType {
+    pub id: EntityTypeId,
+    pub name: EntityTypeName
 }
 
 pub type EntityId = String;
 pub type EntityName = String;
+
+pub type EntityTypeId = String;
+pub type EntityTypeName = String;
